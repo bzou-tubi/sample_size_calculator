0a53ef71bb20560db8f2d159c7fc60e79639578a
Billy Zou <bzou@tubi.tv>
2021-02-11 19:22:57 +0000

    add README

--- ssc_utils/calculator.py
+++ ssc_utils/calculator.py
@@ -1,3 +1,6 @@
+from statsmodels.stats.power import tt_ind_solve_power
+import numpy as np
+
 # Set of helper functions that do the power tests
 
 # Functions that set default parameters
@@ -16,4 +19,15 @@ def power(x=0.8):
 
 def alpha(x=0.05):
     return x
+
+
+def sample_power_ttest(p1, p2, sd_diff, alpha=0.05, power=0.8, ratio=1, alternative = 'two-sided'):
+    mean_diff = abs(p2 - p1)
+    std_effect_size = np.divide(mean_diff, sd_diff)
+    n = tt_ind_solve_power(effect_size=std_effect_size, 
+                         alpha=alpha, 
+                         power=power, 
+                         ratio=ratio, 
+                         alternative=alternative) # Potential improvement: make this able to handle one-sided tests
+    return np.array(n).round()
 
